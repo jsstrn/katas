bdcd51b95ae65b0c841deb312997ce17fbca90dd
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-10-03 11:48:22 +0800

    Add solution to who likes this

--- js/whoLikesThis/whoLikesThis.js
+++ js/whoLikesThis/whoLikesThis.js
@@ -1,5 +1,17 @@
 function whoLikesThis(names) {
-  return "no one likes this";
+  const everyone = {
+    0: "no one",
+    1: `${names[0]}`,
+    2: `${names[0]} and ${names[1]}`,
+    3: `${names[0]}, ${names[1]} and ${names[2]}`,
+    4: `${names[0]}, ${names[1]} and ${names.length - 2} others`
+  };
+
+  let key = Math.min(4, names.length)
+  let someone = everyone[key];
+  let likes = names.length < 2 ? "likes" : "like";
+
+  return `${someone} ${likes} this`;
 }
 
 module.exports = whoLikesThis;
